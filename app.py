0c670b6a2236707590e3dd4e4ac3e511004ff8b4
Sanket <sanketkalode123@gmail.com>
2022-05-23 14:35:28 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'test'
+
+
+@app.route('/')
+def home():
+    return jsonify(message='Welcome to flask app')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ test.py
@@ -0,0 +1,21 @@
+import unittest
+
+from app import app
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self) -> None:
+        self.app_context.pop()
+
+    def test_app(self):
+        self.assertTrue(self.app.config['TESTING'])
+
+
+if __name__ == '__main__':
+    unittest.main()
