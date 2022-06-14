83d1b004863995e53962d9b89940975e3aae3ac5
Sanket-Kalode <Sanket.Kalode@kyndryl.com>
2022-06-14 21:05:07 +0530

    x-ray integration

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, jsonify
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'test'
+xray_recorder.configure(service='My application')
+XRayMiddleware(app, xray_recorder)
 
 
 @app.route('/')
 def home():
     return jsonify(message='Welcome to flask app')
+
+
+@app.route('/<string>')
+def reverse_string(string):
+    data = string[::-1]
+    return jsonify(reverse_string=data)
 
 
 if __name__ == '__main__':
